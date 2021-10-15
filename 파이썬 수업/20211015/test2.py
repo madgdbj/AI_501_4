efa416d7ab86290403a7d67ebf42127b6cc3278e
madgdbj <92001682+madgdbj@users.noreply.github.com>
2021-10-15 16:08:06 +0900

    Add files via upload

---
+++ 파이썬 수업/20211015/test2.py
@@ -0,0 +1,14 @@
+def MH():
+    return 1+2+3+4+5
+
+def doA(repeat):
+    print(repeat)
+    print(repeat)
+    print(repeat)
+
+def doB(num):
+    return num * -1
+
+
+def doC(num1, num2):
+    return (num1 + num2)/2
